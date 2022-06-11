4cf131c16dab9272bb033031db0a6e4ed520841a
UO277188 <uo277188@uniovi.es>
2022-06-11 15:59:48 +0200

    Primer comando

---
+++ commands/cum.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cum')
+		.setDescription('Cumiarse bien de hhhrico'),
+	async execute(interaction) {
+		await interaction.reply('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dairy.com%2Fwp-content%2Fuploads%2F2012%2F12%2FFotolia_24497554_L.jpg&f=1&nofb=1');
+	},
+};
