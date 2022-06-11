73aaf627250eb2e6e24f5e9de4c26a33584c479f
UO277188 <uo277188@uniovi.es>
2022-06-11 18:44:17 +0200

    Actividad

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('Ready!');
+    client.user.setActivity('Cum Simulator 2022', { type: 'PLAYING' });
 });
 
 client.on('interactionCreate', async interaction => {
