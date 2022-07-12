28cdd02893440f8e3a0164f794ceb1644aee6156
diego25c <diegochavalin@gmail.com>
2022-07-12 23:45:06 +0200

    triline

---
+++ commands/triline.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const { joinVoice, playVoiceFakeYou } = require("../shared/voice.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("triline")
+		.setDescription("Triline te habla")
+		.addStringOption(option =>
+			option.setName("mensaje")
+				.setDescription("Mensaje")
+				.setRequired(true)),
+	async execute(interaction) {
+
+		const message = interaction.options.getString("mensaje");
+		if (!message) {
+			interaction.reply("pero poné mensaje boludo").catch(console.error);
+			return;
+		}
+        
+        // token auronplay
+        let voice = "TM:5x6m0n6wqtpw"
+
+        joinVoice(interaction);
+        await playVoiceFakeYou(interaction, voice, message);
+
+	}
+};
