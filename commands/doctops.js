38440c0d8f20472ae9d90dffead9859e51064da0
diego25c <diegochavalin@gmail.com>
2022-07-16 01:20:13 +0200

    doctops

---
+++ commands/doctops.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const { joinVoice, playVoiceFakeYou } = require("../shared/voice.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("doctops")
+		.setDescription("Doctops te habla")
+		.addStringOption(option =>
+			option.setName("mensaje")
+				.setDescription("Mensaje")
+				.setRequired(true)),
+	async execute(interaction) {
+
+		const message = interaction.options.getString("mensaje");
+		if (!message) {
+			interaction.reply("pero poné mensaje boludo").catch(console.error);
+			return;
+		}
+        
+        // token doctops
+        let voice = "TM:2wx5r4sxkry1"
+
+        joinVoice(interaction);
+        await playVoiceFakeYou(interaction, voice, message);
+
+	}
+};
