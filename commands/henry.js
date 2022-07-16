804c720b2f708cd7512a15ee34706e0c13847699
diego25c <diegochavalin@gmail.com>
2022-07-16 18:24:55 +0200

    mas henry

--- commands/henry.js
+++ commands/henry.js
@@ -1,7 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { joinVoice, playVoiceLocal, playVoice, leaveVoice } = require("../shared/voice.js");
 
-const audioPlayers = new Map();
+const audios = [];
+const audiosPath = path.join(__dirname, 'resources');
+const audioFiles = fs.readdirSync(audiosPath).filter(file => file.endsWith('.mp3'));
+
+for (const file of audioFiles) {
+	const filePath = path.join(audiosPath, file);
+	const audio = require(filePath);
+	audios.push(file.data.toJSON());
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +18,7 @@ module.exports = {
 	async execute(interaction) {
 		console.log("henry");
         joinVoice(interaction);
-        await playVoiceLocal(interaction, "henry.mp3");
+		let audioName = audios[Math.floor(Math.random()*items.length)];
+        await playVoiceLocal(interaction, audioName);
     },
 };
